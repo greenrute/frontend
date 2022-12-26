93384a52a1563e957c34bf80c8a3ac1e3a4f2e80
Nazar Niphone <ndzobas@gmail.com>
2022-12-26 21:44:13 +0200

    Додано атрибут мови

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -37,6 +37,9 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
+      htmlAttrs: {
+        lang: 'uk',
+      },
       link: [
         { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
         { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
