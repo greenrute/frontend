cdb16dd3207e2eb460f7ca90cbd49899d3314d7f
Nazar Niphone <ndzobas@gmail.com>
2023-01-10 16:11:22 +0200

    Додано перемикач мови

---
+++ components/Main/LangSwitcher.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import {Menu, MenuButton, MenuItem, MenuItems} from '@headlessui/vue'
+import {LanguageIcon} from '@heroicons/vue/20/solid/index'
+
+const {locale, locales, setLocale} = useI18n()
+</script>
+
+<template>
+  <Menu as="div" class="relative inline-block text-left">
+    <MenuButton :as="resolveComponent('MainButton')" class="px-2" color="zinc" variant="outline">
+      <LanguageIcon aria-hidden="true" class="h-5 w-5" />
+    </MenuButton>
+
+    <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
+      <MenuItems class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <div class="py-1">
+          <MenuItem v-for="l in locales" :key="l.code" v-slot="{ active, close }" :disabled="l.code === locale">
+            <button @click="setLocale(l.code);close" class="flex items-center w-full gap-1.5 px-4 py-2 text-sm" :class="[
+              active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+              l.code === locale && 'opacity-50 pointer-events-none',
+            ]">
+              <component :is="'Emoji' + l.icon" class="h-4 w-4 shrink-0" />
+              {{ l.name }}
+            </button>
+          </MenuItem>
+        </div>
+      </MenuItems>
+    </transition>
+  </Menu>
+</template>
