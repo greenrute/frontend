a659c49e097a420b675161424cd931ee24262291
Nazar Niphone <ndzobas@gmail.com>
2023-01-10 16:11:49 +0200

    Додано піктограму української мови

---
+++ components/global/Emoji/Ukrainian.vue
@@ -0,0 +1,6 @@
+<template>
+  <svg viewBox="0 0 36 36" xmlns="http://www.w3.org/2000/svg">
+    <path d="M32 5H4C1.791 5 0 6.791 0 9v9h36V9c0-2.209-1.791-4-4-4z" fill="#005BBB" />
+    <path d="M36 27c0 2.209-1.791 4-4 4H4c-2.209 0-4-1.791-4-4v-9h36v9z" fill="#FFD500" />
+  </svg>
+</template>
