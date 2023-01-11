bfbac52df20a144e199e915aa34517781c45bd3b
Nazar Niphone <74293442+nazar1ua@users.noreply.github.com>
2023-01-11 20:45:15 +0200

    Виправлено помилку

--- components/Main/LangSwitcher.vue
+++ components/Main/LangSwitcher.vue
@@ -3,7 +3,7 @@ import {Menu, MenuButton, MenuItem, MenuItems} from '@headlessui/vue'
 import {LanguageIcon} from '@heroicons/vue/20/solid/index'
 
 const {locale, locales, setLocale} = useI18n()
-await preloadComponents(locales.map(l => 'Emoji' + l.icon))
+await preloadComponents(locales.value.map(l => 'Emoji' + l.icon))
 </script>
 
 <template>
