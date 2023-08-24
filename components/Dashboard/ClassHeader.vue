259ed9504aa9c8aa70641a6e8e407bfd01bc5b55
Nazar Niphone <74293442+nazar1ua@users.noreply.github.com>
2023-08-24 23:46:41 +0300

    Покращення шапки

--- components/Dashboard/ClassHeader.vue
+++ components/Dashboard/ClassHeader.vue
@@ -5,25 +5,26 @@ import { InformationCircleIcon as InformationCircleOutlineIcon } from '@heroicon
 import { InformationCircleIcon as InformationCircleSolidIcon } from '@heroicons/vue/24/solid'
 
 const route = useRoute()
+const localePath = useLocalePath()
 const currentClass = useCurrentClass()
 </script>
 
 <template>
   <div class="hidden lg:flex border-b border-gray-200 dark:border-zinc-700 py-3 items-center justify-between px-8">
-    <div class="mt-4 flex items-center gap-2 sm:mt-0">
+    <NuxtLink :to="localePath(`/c/${currentClass.hash}`)" class="mt-4 flex items-center gap-2 sm:mt-0">
       <div class="h-3 w-3 rounded-full" :style="{ backgroundColor: currentClass.color }" />
       <h3 class="text-lg">{{ currentClass.name }}</h3>
-    </div>
+    </NuxtLink>
     <nav>
       <ul class="flex items-center gap-4">
         <li>
-          <NuxtLink :to="`/c/${currentClass.hash}`" :class="route.name === 'c-hash' ? 'font-bold' : ''">{{ $t('menu.schedule') }}</NuxtLink>
+          <NuxtLink :to="localePath(`/c/${currentClass.hash}`)" :class="route.name === 'c-hash' ? 'font-bold' : ''">{{ $t('menu.schedule') }}</NuxtLink>
         </li>
       </ul>
     </nav>
 
     <div class="flex items-center gap-1.5">
-      <NuxtLink :to="`/c/${currentClass.hash}/edit`" class="text-gray-600 hover:text-gray-800 dark:text-zinc-200 dark:hover:text-white">
+      <NuxtLink :to="localePath(`/c/${currentClass.hash}/edit`)" class="text-gray-600 hover:text-gray-800 dark:text-zinc-200 dark:hover:text-white">
         <span class="sr-only">{{ $t('menu.edit class info') }}</span>
         <InformationCircleSolidIcon class="h-6 w-6 text-gray-800 dark:text-white" aria-hidden="true" v-if="route.name === 'c-hash-edit'" />
         <InformationCircleOutlineIcon class="h-6 w-6" aria-hidden="true" v-else />
@@ -40,7 +41,7 @@ const currentClass = useCurrentClass()
           <MenuItems class="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white dark:bg-zinc-800 shadow-lg ring-1 ring-black ring-opacity-5 dark:ring-zinc-700 focus:outline-none">
             <div class="py-1">
               <MenuItem v-slot="{ active }">
-              <NuxtLink :to="`/c/${currentClass.hash}/schedule`" class="group flex items-center px-4 py-2 text-sm" :class="active ? 'bg-gray-100 dark:bg-zinc-900 text-gray-900 dark:text-zinc-50' : 'text-gray-800 dark:text-zinc-300'">
+              <NuxtLink :to="localePath(`/c/${currentClass.hash}/schedule`)" class="group flex items-center px-4 py-2 text-sm" :class="active ? 'bg-gray-100 dark:bg-zinc-900 text-gray-900 dark:text-zinc-50' : 'text-gray-800 dark:text-zinc-300'">
                 <PencilSquareIcon class="mr-3 h-5 w-5 text-gray-500 dark:text-zinc-400 group-hover:text-gray-600 dark:group-hover:text-zinc-400" aria-hidden="true" />
                 {{ $t('menu.edit schedule') }}
               </NuxtLink>
